95d4bfdcdc08ff9d10b25d0e84f6a6fdd6baa40f
Wootae94 <rxd0414@gmail.com>
2020-11-05 17:53:05 +0900

    Unit.38-45

---
+++ u45_모듈/main2.py
@@ -0,0 +1,7 @@
+from calcpkg import * 
+ 
+print(calcpkg.operation.add(10, 20))    
+print(calcpkg.operation.mul(10, 20))    
+ 
+print(calcpkg.geometry.triangle_area(30, 40))    
+print(calcpkg.geometry.rectangle_area(30, 40))   
